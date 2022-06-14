d80baa59e5fd4e0cd2bba16e701cd0e6b86232c3
halforweb <halforweb@gmail.com>
2022-06-14 12:01:45 +0200

    products added to the homepage

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,36 @@
-fetch("http://localhost:3000/api/products")
-    .then(response => response.json())
-    .then(response2 => console.log(response2[1]))
-
+document.addEventListener('DOMContentLoaded', ()=>{
+    function fetchData() {
+        fetch('http://localhost:3000/api/products')
+        .then(resp => resp.json())
+        .then(data => renderProducts(data))
+      }
+      function renderProducts(data) {
+          for (const q of data) {
     
+          //Define the container where we attach everything to
+          const item = document.querySelector('#items');
+    
+          //Create all necessary elements
+          const itemsLink = document.createElement('a');
+          const itemsArticle = document.createElement('article');
+          const itemsImg = document.createElement('img');
+          const itemsTitle = document.createElement('h3');
+          const itemsDescription = document.createElement('p');
+    
+          //Grab data and insert it into created elements
+          itemsLink.href = `../html/product.html?id=${q._id}`;
+          itemsImg.src = q.imageUrl;
+          itemsImg.alt = q.altTxt;
+          itemsTitle.innerText = q.name;
+          itemsDescription.innerText = q.description;
+    
+          //Append everything to main container
+          itemsArticle.append(itemsImg, itemsTitle, itemsDescription);
+          itemsLink.append(itemsArticle);
+          item.append(itemsLink);
+          }
+       }
+    
+       //Call the function that will automatically run renderProducts() also 
+       fetchData();
+    })
