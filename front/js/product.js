ff2bf6ac03d569b315003dedf2d0d133b313b73b
halforweb <halforweb@gmail.com>
2022-06-17 13:53:24 +0200

    add product to the card section added to product.js with local storage set up

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
       
-            
+     //******************************************************************************* Display the product in the webpage  ********************************************************** */       
             // Function to get the url parameter of the specified key
             getParameter = (key) => {
                 address = window.location.search
@@ -17,6 +17,7 @@
                 .then(resp => resp.json())
                 .then(dataId => renderProduct(dataId))
             }
+            
             // Insert product details 
             function renderProduct(dataId) {
             
@@ -46,14 +47,80 @@
                         itemColorsBloc.appendChild(itemColors);
                     }
                }
+ 
+     //******************************************************************************* Add product in the card  ********************************************************** */       
+   
+               // Save the product selected in local storage
+                function addToCard() {
+
+                    // Check the button click "add to card"
+                    const button = document.getElementById("addToCart");
+                          button.addEventListener("click", function () {
+
+                
+                            // Define the product details
+                            const selectedProduct = {
+                                idSelectedProduct: idItem,
+                                colorSelectedProduct: document.getElementById("colors").value,
+                                quantitySelectedProduct: document.getElementById("quantity").value,
+                                };
+
+                            // Control the product details selected
+                            if (selectedProduct.quantitySelectedProduct > 100 || selectedProduct.quantitySelectedProduct < 1 ) {
+                                alert('Veuillez renseigner une quantité entre 1 et 100');
+                                }
+                            else if (selectedProduct.colorSelectedProduct == "") { 
+                                    alert("Veuillez selectionner une couleur");
+                                }
+
+                            // Create the local storage
+                            let selectedProductArray = JSON.parse(localStorage.getItem("selectedProduct"));
+
+                            
+                            // Amend the local storage if there is no product
+                            if (selectedProductArray == null) { 
+                                selectedProductArray = [];
+                                selectedProductArray.push(selectedProduct);
+                                localStorage.setItem("selectedProduct", JSON.stringify(selectedProductArray));
+                                alert("Produit ajouté au panier !");
+                            }
+                            // Amend the local storage if there is existing product
+                            else if (selectedProductArray != null) {
             
-               //Call the function that will automatically run renderProduct() also 
-               fetchDataId();
+                                // Check the local storage
+                                let findProducts = selectedProductArray.find((element) =>
+                                        element.idSelectedProduct === selectedProduct.idSelectedProduct
+                                        &&
+                                        element.colorSelectedProduct === selectedProduct.colorSelectedProduct
+                                );
+            
+                                // if there is an existing product with same ID and Color, increment quantity to the local storage  
+                                if (findProducts) {
+            
+                                    let newQuantity = parseInt(findProducts.quantitySelectedProduct) + parseInt(selectedProduct.quantitySelectedProduct);
+                                    findProducts.quantitySelectedProduct = newQuantity;
+                                    if(newQuantity > 100) {
+                                        newQuantity = 100;
+                                    }
+                                    localStorage.setItem("selectedProduct", JSON.stringify(selectedProductArray));
+                                    alert(`${findProducts.quantitySelectedProduct} exemplaire(s) de ce produit sont dans le panier`)
+                                }
+            
+                                // if there is no existing product with the same ID and color, the new product is added to the local storage
+                                else {
+                                    selectedProductArray.push(selectedProduct);
+                                    localStorage.setItem("selectedProduct", JSON.stringify(selectedProductArray));
+                                    alert(`${selectedProduct.quantitySelectedProduct} exemplaire(s) de ce produit ajouté(s) au panier !`)
+                                }
+                            }
+                        }
+                        )                       
+                   }
 
-              
-              
-               
 
+//Call the functions 
+    fetchDataId();
+    addToCard();
               
 
 
